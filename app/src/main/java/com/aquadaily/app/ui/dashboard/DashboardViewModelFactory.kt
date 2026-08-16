307c5c95f8e3a1fd4c2030d3845337cf86d0da95
Alfarisky <riskymuhammadalfa2@gmail.com>
2026-08-16 13:33:13 +0700

    refactor(dashboard): inject current user into viewmodel

--- app/src/main/java/com/aquadaily/app/ui/dashboard/DashboardViewModelFactory.kt
+++ app/src/main/java/com/aquadaily/app/ui/dashboard/DashboardViewModelFactory.kt
@@ -7,12 +7,18 @@ import com.aquadaily.app.core.repository.ReminderRepository
 
 class DashboardViewModelFactory(
     private val historyRepository: HistoryRepository,
-    private val reminderRepository: ReminderRepository
+    private val reminderRepository: ReminderRepository,
+    private val userId: Int
 ) : ViewModelProvider.Factory {
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DashboardViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return DashboardViewModel(historyRepository, reminderRepository) as T
+            return DashboardViewModel(
+                historyRepository,
+                reminderRepository,
+                userId
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
